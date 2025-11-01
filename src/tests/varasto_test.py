dc8682a639e4282661d11b82f9471fedfe6c35a8
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-11-02 00:02:50 +0200

    Hurraa tehtävä 8 tehty, 100% coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,34 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisaa_yli_tilavuuden(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_poista_yli_saldon(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu = self.varasto.ota_varastosta(1000)
+
+        self.assertEqual(self.varasto.saldo, 0)
+        self.assertEqual(saatu, 8)
+
+    def test_luo_liian_pieni_varasto(self):
+        varasto2 = Varasto(-10)
+        self.assertEqual(varasto2.tilavuus, 0)
+
+    def test_luo_liian_matalalla_saldolla_varasto(self):
+        varasto2 = Varasto(12, -100)
+        self.assertEqual(varasto2.saldo, 0)
+
+    def test_lisaa_alle_nolla_asiaa(self):
+        self.varasto.lisaa_varastoon(-15)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_ota_varastosta_negatiivinen(self):
+        self.varasto.ota_varastosta(-15)
+        self.assertEqual(self.varasto.saldo, 0)
+
+    def test_teskti_on_oikein(self):
+        self.varasto.lisaa_varastoon(6)
+        self.assertEqual(self.varasto.__str__(), f"saldo = 6, vielä tilaa 4")
